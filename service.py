f9991362d6f315e029e46cfe7050451e872cbf3e
Damian Moore <damian@epixstudios.co.uk>
2015-04-29 21:49:31 +0100

    Simulated output controlled by serial interface

---
+++ service.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+import serial
+
+from settings import structure
+from menu import Menu
+
+
+def main():
+    ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=0)
+
+    menu = Menu(structure, 20, 4)
+
+    def show():
+        for i in range(20):
+            print('')
+        menu.show()
+
+    show()
+
+    while True:
+        data = ser.read(255).strip()
+        if data:
+            if data == 'c':     # clockwise
+                menu.down()
+            elif data == 'a':   # anti-clockwise
+                menu.up()
+            elif data == 'p':   # press
+                menu.select()
+            show()
+        sleep(0.1)
+
+
+if __name__ == '__main__':
+    main()
